82ba92cc621d52bbf3e9193ba2e7cb9e89b289d8
Ali Alnujaidi <Ali.Alnujaidi4@gmail.com>
2023-05-08 23:19:15 +0300

    ..

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {updateEvaluation} from '../Controllers/adminController.js'
+import {updateEvaluation} from '../Controllers/AdminController.js'
 import { requireAdmin } from '../middleware/authMiddleware.js';
 const router = express.Router()
 
